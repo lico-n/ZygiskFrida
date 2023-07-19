4fe0ea89148b1488a0496992fca8e573ce1812b8
lico-n <85761230+lico-n@users.noreply.github.com>
2023-07-19 18:46:37 +0200

    clean up the tmp directory

--- template/magisk_module/customize.sh
+++ template/magisk_module/customize.sh
@@ -4,6 +4,8 @@ mkdir -p \$TMP_MODULE_DIR
 
 cp \$MODPATH/gadget/libgadget-\$ARCH.so.xz \$TMP_MODULE_DIR/${gadgetLibraryName}.xz
 /data/adb/magisk/busybox unxz \$TMP_MODULE_DIR/${gadgetLibraryName}.xz
+rm \$TMP_MODULE_DIR/${gadgetLibraryName}.xz
+
 touch \$TMP_MODULE_DIR/target_packages
 
 set_perm_recursive \$TMP_MODULE_DIR 0 0 0755 0644
