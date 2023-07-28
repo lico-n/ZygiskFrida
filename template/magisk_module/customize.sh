c2f804e2886bcc0c186b540cb5a1664efd489644
lico-n <85761230+lico-n@users.noreply.github.com>
2023-07-28 18:30:10 +0200

    make module install leaner

--- template/magisk_module/customize.sh
+++ template/magisk_module/customize.sh
@@ -1,11 +1,65 @@
+SKIPUNZIP=1
+
 TMP_MODULE_DIR=/data/local/tmp/${modulePackageName}
 
-mkdir -p \$TMP_MODULE_DIR
+if [ "\$ARCH" != "arm" ] && [ "\$ARCH" != "arm64" ] && [ "\$ARCH" != "x86" ] && [ "\$ARCH" != "x64" ]; then
+  abort "! Unsupported platform: \$ARCH"
+else
+  ui_print "- Device platform: \$ARCH"
+fi
 
-cp \$MODPATH/gadget/libgadget-\$ARCH.so.xz \$TMP_MODULE_DIR/${gadgetLibraryName}.xz
-/data/adb/magisk/busybox unxz \$TMP_MODULE_DIR/${gadgetLibraryName}.xz
-rm \$TMP_MODULE_DIR/${gadgetLibraryName}.xz
+extract() {
+  zip=\$1
+  file=\$2
+  dir=\$3
 
-touch \$TMP_MODULE_DIR/target_packages
+  unzip -oj "\$zip" "\$file" -d "\$dir" >&2
+}
 
-set_perm_recursive \$TMP_MODULE_DIR 0 0 0755 0644
+mkdir "\$MODPATH/zygisk"
+mkdir -p "\$TMP_MODULE_DIR"
+extract "\$ZIPFILE" 'module.prop' "\$MODPATH"
+extract "\$ZIPFILE" 'uninstall.sh' "\$MODPATH"
+
+if [ "\$ARCH" = "arm" ]  || [ "\$ARCH" = "arm64" ]; then
+  ui_print "- Extracting arm libraries"
+  extract "\$ZIPFILE" "zygisk/armeabi-v7a.so" "\$MODPATH/zygisk"
+
+  if [ "\$IS64BIT" = true ]; then
+    ui_print "- Extracting arm64 libraries"
+    extract "\$ZIPFILE" "zygisk/arm64-v8a.so" "\$MODPATH/zygisk"
+    extract "\$ZIPFILE" "gadget/libgadget-arm64.so.xz" "\$TMP_MODULE_DIR"
+    mv "\$TMP_MODULE_DIR/libgadget-arm64.so.xz" "\$TMP_MODULE_DIR/${gadgetLibraryName}.xz"
+  else
+    extract "\$ZIPFILE" "gadget/libgadget-arm.so.xz" "\$TMP_MODULE_DIR"
+    mv "\$TMP_MODULE_DIR/libgadget-arm.so.xz" "\$TMP_MODULE_DIR/${gadgetLibraryName}.xz"
+  fi
+fi
+
+if [ "\$ARCH" = "x86" ] || [ "\$ARCH" = "x64" ]; then
+  ui_print "- Extracting x86 libraries"
+  extract "\$ZIPFILE" "zygisk/x86.so" "\$MODPATH/zygisk"
+
+  if [ "\$IS64BIT" = true ]; then
+    ui_print "- Extracting x64 libraries"
+    extract "\$ZIPFILE" "zygisk/x86_64.so" "\$MODPATH/zygisk"
+    extract "\$ZIPFILE" "gadget/libgadget-x64.so.xz" "\$TMP_MODULE_DIR"
+    mv "\$TMP_MODULE_DIR/libgadget-x64.so.xz" "\$TMP_MODULE_DIR/${gadgetLibraryName}.xz"
+  else
+    extract "\$ZIPFILE" "gadget/libgadget-x86.so.xz" "\$TMP_MODULE_DIR"
+    mv "\$TMP_MODULE_DIR/libgadget-x86.so.xz" "\$TMP_MODULE_DIR/${gadgetLibraryName}.xz"
+  fi
+fi
+
+/data/adb/magisk/busybox unxz "\$TMP_MODULE_DIR/${gadgetLibraryName}.xz"
+rm "\$TMP_MODULE_DIR/${gadgetLibraryName}.xz"
+
+touch "\$TMP_MODULE_DIR/target_packages"
+
+set_perm_recursive "\$TMP_MODULE_DIR" 0 0 0755 0644
+set_perm_recursive "\$MODPATH" 0 0 0755 0644
+
+
+
+
+
