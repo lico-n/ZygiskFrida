ccfa14da7a33b6b681ef0ab87c0ddfa410d5102b
lico-n <85761230+lico-n@users.noreply.github.com>
2023-08-05 17:02:30 +0200

    feat: add support to inject arbitrary libraries

--- module/src/jni/inject.cpp
+++ module/src/jni/inject.cpp
@@ -4,9 +4,11 @@
 #include <cinttypes>
 #include <filesystem>
 #include <fstream>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <thread>
+#include <utility>
 
 #include "config.h"
 #include "log.h"
@@ -22,7 +24,7 @@ static std::string get_process_name() {
     return buffer.str();
 }
 
-static void wait_for_init(std::string const& app_name) {
+static void wait_for_init(std::string const &app_name) {
     LOGI("Wait for process to complete init");
 
     // wait until the process is renamed to the package name
@@ -36,15 +38,15 @@ static void wait_for_init(std::string const& app_name) {
     LOGI("Process init completed");
 }
 
-static void delay_start_up(uint64_t start_up_delay) {
-    if (start_up_delay <= 0) {
+static void delay_start_up(uint64_t start_up_delay_ms) {
+    if (start_up_delay_ms <= 0) {
         return;
     }
 
-    LOGI("Waiting for configured start up delay %" PRIu64"ms", start_up_delay);
+    LOGI("Waiting for configured start up delay %" PRIu64"ms", start_up_delay_ms);
 
     int countdown = 0;
-    uint64_t delay = start_up_delay;
+    uint64_t delay = start_up_delay_ms;
 
     for (int i = 0; i < 10 && delay > 1000; i++) {
         delay -= 1000;
@@ -54,32 +56,33 @@ static void delay_start_up(uint64_t start_up_delay) {
     std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 
     for (int i = countdown; i > 0; i--) {
-        LOGI("Starting gadget in %d seconds", i);
+        LOGI("Injecting libs in %d seconds", i);
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
 
-void inject_gadget(std::string const gadget_path, std::unique_ptr<target_config> cfg) {
+void inject_libs(std::unique_ptr<target_config> cfg) {
     // We need to wait for process initialization to complete.
     // Loading the gadget before that will freeze the process
     // before the init has completed. This make the process
     // undiscoverable or otherwise cause issue attaching.
     wait_for_init(cfg->app_name);
 
-    delay_start_up(cfg->start_up_delay);
+    delay_start_up(cfg->start_up_delay_ms);
 
-    LOGI("Starting gadget %s", gadget_path.c_str());
-    auto *handle = xdl_open(gadget_path.c_str(), XDL_ALWAYS_FORCE_LOAD);
-    if (handle) {
-        LOGI("Gadget connected");
-    } else {
-        LOGE("Failed to start gadget: %s", dlerror());
+    for (auto & lib_path : cfg->injected_libraries) {
+        LOGI("Injecting %s", lib_path.c_str());
+        auto *handle = xdl_open(lib_path.c_str(), XDL_TRY_FORCE_LOAD);
+        if (handle) {
+            LOGI("Injected %s", lib_path.c_str());
+        } else {
+            LOGE("Failed to inject %s : %s", lib_path.c_str(), dlerror());
+        }
     }
 }
 
-bool check_and_inject(std::string const& app_name) {
+bool check_and_inject(std::string const &app_name) {
     std::string module_dir = std::string("/data/local/tmp/re.zyg.fri");
-    std::string gadget_path = module_dir + "/libgadget.so";
 
     std::unique_ptr<target_config> cfg = load_config(module_dir, app_name);
     if (cfg == nullptr) {
@@ -88,7 +91,13 @@ bool check_and_inject(std::string const& app_name) {
 
     LOGI("App detected: %s", app_name.c_str());
 
-    std::thread inject_thread(inject_gadget, gadget_path, std::move(cfg));
+    if (cfg->injected_libraries.empty()) {
+        LOGI("No libraries configured for injection. "
+             "Check the content of the `injected_libraries` file or delete it.");
+        return false;
+    }
+
+    std::thread inject_thread(inject_libs, std::move(cfg));
     inject_thread.detach();
 
     return true;
