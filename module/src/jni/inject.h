1967d7c779a32387780484899553b05d66167794
lico-n <85761230+lico-n@users.noreply.github.com>
2023-11-23 00:04:55 +0100

    add dlopen as fallback to xdl_open

--- module/src/jni/inject.h
+++ module/src/jni/inject.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+void inject_lib(std::string const& lib_path, std::string const& logContext);
 bool check_and_inject(std::string const& app_name);
 
 #endif  // ZYGISKFRIDA_INJECT_H
